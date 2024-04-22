9a922607dda21229e8a1c27560b75f31edc36a9e
KFry101 <97113447+KFry101@users.noreply.github.com>
2024-04-22 13:51:00 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -3,22 +3,28 @@
 using namespace std;
 
 ShoppingCart::ShoppingCart(){
+  customerName="";
+  currentDate="";  
   
 }
 
 ShoppingCart::ShoppingCart(string name, string date){
+   customerName=name;
+   currentDate=date; 
   
 }
 
 string ShoppingCart::GetCustomerName() const{
+  return customerName;
   
 }
 
 string ShoppingCart::GetDate() const{ 
-
+  return currentDate;
 }
       
 void ShoppingCart::AddItem(ItemToPurchase item){
+  cartItems.push_back(item);
 
 }
 
@@ -31,15 +37,25 @@ void ShoppingCart::ModifyItem(ItemToPurchase item){
 }
       
 int ShoppingCart::GetNumItemsInCart(){
+  return cartItems.lenght();
   
 }
 
 double ShoppingCart::GetCostOfCart(){
-  
+  double cartPrice=0.0;
+  for(ItemToPurchase e:cartItems){
+    cartPrice= e.GetPrice();
+  }
+  return cartPrice;
 }
       
 void ShoppingCart::PrintTotal(){
-  
+  if(cartItems.length() ==0){
+    cout<<"SHOPPING CART IS EMPTY"<<endl;
+  }
+  else{
+    cout<<cartItems.GetCostOfCart<<endl;
+  }
 }
 
 void ShoppingCart::PrintDescriptions(){
