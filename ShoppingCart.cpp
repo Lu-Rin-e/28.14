d7f8614109cfe26cb3b0119ad08814e3088d05af
KFry101 <97113447+KFry101@users.noreply.github.com>
2024-04-22 13:26:11 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,47 @@
-/* Type your code here */
+#include <iostream>
+#include "ShoppingCart.h"
+using namespace std;
+
+ShoppingCart::ShoppingCart(){
+  
+}
+
+ShoppingCart::ShoppingCart(string name, string date){
+  
+}
+
+string ShoppingCart::GetCustomerName() const{
+  
+}
+
+string ShoppingCart::GetDate() const{ 
+
+}
+      
+void ShoppingCart::AddItem(ItemToPurchase item){
+
+}
+
+void ShoppingCart::RemoveItem(string name){
+  
+}
+      
+void ShoppingCart::ModifyItem(ItemToPurchase item){
+  
+}
+      
+int ShoppingCart::GetNumItemsInCart(){
+  
+}
+
+double ShoppingCart::GetCostOfCart(){
+  
+}
+      
+void ShoppingCart::PrintTotal(){
+  
+}
+
+void ShoppingCart::PrintDescriptions(){
+  
+}
